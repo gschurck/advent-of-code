b0f3ac290886665ed0fcde8b7d9846c818cbcd69
Guillaume Schurck <g.schurck@gmail.com>
2022-12-04 20:51:19 +0100

    add day2 go version

---
+++ aoc2022/day2/day2.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var rules = [3]rune{'A', 'B', 'C'}
+var points int
+
+func main() {
+	for _, r := range rules {
+		fmt.Printf("%v wins against %v\n", string(rules[getId(r)]), string(winsAgainst(r)))
+		fmt.Printf("%v loses against %v\n", string(rules[getId(r)]), string(losesAgainst(r)))
+	}
+
+	inputScanner := getInput(2)
+
+	for inputScanner.Scan() {
+		//println(inputScanner.Text())
+		runes := []rune(inputScanner.Text())
+		if len(runes) < 3 {
+			continue
+		}
+		processRound(runes[0], runes[2])
+	}
+
+	fmt.Printf("\nPoints: %v\n", points)
+}
+
+func processRound(letter1 rune, letter2 rune) {
+	var expectedLetter rune
+
+	switch letter2 {
+	case 'X':
+		expectedLetter = winsAgainst(letter1)
+	case 'Y':
+		expectedLetter = letter1
+	case 'Z':
+		expectedLetter = losesAgainst(letter1)
+	}
+
+	letter2 = expectedLetter
+	letter2Id := getId(letter2)
+	letter2 = rules[letter2Id]
+	loserLetter := winsAgainst(letter2)
+	if loserLetter == letter1 {
+		points += 6
+	} else if letter1 == letter2 {
+		points += 3
+	}
+	points += letter2Id + 1
+}
+
+func getId(letter rune) int {
+	i := letter - 65
+	i = i % 23
+	return int(i)
+}
+
+func winsAgainst(letter rune) rune {
+	id := getId(letter)
+	opponentId := mod(id-1, 3)
+	return rules[opponentId]
+}
+
+func losesAgainst(letter rune) rune {
+	id := getId(letter)
+	opponentId := (id + 1) % 3
+	return rules[opponentId]
+}
+
+func getInput(day int) *bufio.Scanner {
+	pwd, _ := os.Getwd()
+	b, err := os.ReadFile(pwd + "/day" + strconv.Itoa(day) + "/input.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	str := string(b)
+	return bufio.NewScanner(strings.NewReader(str))
+}
+
+func mod(a, b int) int {
+	return (a%b + b) % b
+}
