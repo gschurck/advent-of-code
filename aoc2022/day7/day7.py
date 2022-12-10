c258f1e1745d1a6478f8f23030c928b8e0772c97
Guillaume Schurck <g.schurck@gmail.com>
2022-12-10 17:40:06 +0100

    add day 7 star 2

--- aoc2022/day7/day7.py
+++ aoc2022/day7/day7.py
@@ -6,16 +6,18 @@ from aoc import get_input
 class Directory:
     parent: 'Directory'
     directories: dict[str, 'Directory']
+    size: int
 
     def __init__(self, parent):
         self.parent = parent
         self.directories = {}
-        self.files = []
+        self.files_sizes = []
 
     def get_directory_size(self) -> int:
-        directory_size: int = self.get_subdirectories_size() + sum(self.files)
-        global sum_size_small_directories
-        sum_size_small_directories += directory_size if directory_size <= 100000 else 0
+        directory_size: int = self.get_subdirectories_size() + sum(self.files_sizes)
+        CmdParser.sum_size_small_directories += directory_size if directory_size <= 100000 else 0
+        self.size = directory_size
+        CmdParser.directories_sizes.append(directory_size)
         return directory_size
 
     def get_subdirectories_size(self) -> int:
@@ -28,6 +30,9 @@ class Directory:
 class CmdParser:
     cwd: Directory = Directory(None)
     is_ls: bool = False
+    sum_size_small_directories = 0
+    size_dir_to_delete: int
+    directories_sizes: [int] = []
 
     @staticmethod
     def cd(directory_name: str):
@@ -47,7 +52,7 @@ class CmdParser:
             CmdParser.cwd.directories[directory_name] = Directory(CmdParser.cwd)
         else:
             file_size: int = int(splitted_line[0])
-            CmdParser.cwd.files.append(file_size)
+            CmdParser.cwd.files_sizes.append(file_size)
 
     @staticmethod
     def get_root_recursive(dir: Directory):
@@ -57,12 +62,25 @@ class CmdParser:
     def cd_root():
         CmdParser.cwd = CmdParser.get_root_recursive(CmdParser.cwd)
 
+    # Star 2 **
+    @staticmethod
+    def get_size_directory_to_delete() -> int:
+        CmdParser.directories_sizes.sort()
+        unused_space: int = 70000000 - CmdParser.get_root_total_size()
+        min_size_dir_to_del: int = 30000000 - unused_space
+        return next(size for size in CmdParser.directories_sizes if size > min_size_dir_to_del)
+
+    @staticmethod
+    def get_root_total_size():
+        CmdParser.cd_root()
+        root = CmdParser.cwd
+        return sum([directory.size for directory in root.directories.values()]) + sum(root.files_sizes)
+
 
 line_input = io.StringIO(get_input())
 
 for line in line_input:
     clean_line = line.strip('\n')
-    print(clean_line)
     if clean_line[0] == '$':
         CmdParser.is_ls = False
         match clean_line[2:4]:
@@ -74,7 +92,10 @@ for line in line_input:
         CmdParser.ls(clean_line)
 
 CmdParser.cd_root()
-sum_size_small_directories = 0
 _ = CmdParser.cwd.get_subdirectories_size()
 
-print("Small directories size sum:", sum_size_small_directories)
+# Star 1 *
+print("Small directories size sum:", CmdParser.sum_size_small_directories)
+
+# Star 2 **
+print("Total size of directory to delete:", CmdParser.get_size_directory_to_delete())
