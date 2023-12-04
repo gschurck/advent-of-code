b60c673d20da39dc9291dae8a34e8f4b2e84b76f
Guillaume Schurck <g.schurck@gmail.com>
2023-12-04 16:50:35 +0100

    optimize day 4

--- aoc2023/day4/part1.py
+++ aoc2023/day4/part1.py
@@ -6,15 +6,7 @@ for id_line, line in enumerate(get_input()):
     line = line.strip('\n')
     print(line)
     line = line.split(":")[1]
-    winning_numbers_strings, numbers_strings = line.split("|")
-    winning_numbers_strings = winning_numbers_strings.strip()
-    winning_numbers_strings = winning_numbers_strings.split(" ")
-    numbers_strings = numbers_strings.strip()
-    numbers_strings = numbers_strings.split(" ")
-
-    # remove empty strings
-    winning_numbers_strings = list(filter(None, winning_numbers_strings))
-    numbers_strings = list(filter(None, numbers_strings))
+    winning_numbers_strings, numbers_strings = map(str.split, line.split("|"))
 
     winning_numbers = [int(wn) for wn in winning_numbers_strings]
     numbers = [int(n) for n in numbers_strings]
