4cfcce66145cc1c934a01d3e165533ab5b660b7e
Guillaume Schurck <g.schurck@gmail.com>
2023-12-05 09:15:25 +0100

    quick fixes

--- aoc2023/day1/day1.py
+++ aoc2023/day1/day1.py
@@ -2,7 +2,7 @@ import io
 
 from aoc import get_input
 
-sum = 0
+result = 0
 
 digits = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
 
@@ -21,21 +21,21 @@ def process_char(char, line_part, reversed=False):
 
 
 for line in get_input():
-    clean_line = line.strip('\n')
-    print(clean_line)
+    line = line.strip('\n')
+    print(line)
     line_part = ""
-    for char in clean_line:
+    for char in line:
         char1, line_part = process_char(char, line_part)
         if char1:
             break
 
     line_part = ""
-    for char in reversed(clean_line):
+    for char in reversed(line):
         char2, line_part = process_char(char, line_part, True)
         if char2:
             break
 
     nb = int(char1 + char2)
     print(nb)
-    sum += nb
-print(sum)
+    result += nb
+print(result)
