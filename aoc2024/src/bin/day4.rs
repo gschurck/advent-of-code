9933cde35415e85cf969a2532d5045d082afc0b3
Guillaume Schurck <21091232+gschurck@users.noreply.github.com>
2024-12-07 16:12:32 +0100

    continue day 4 2024

--- aoc2024/src/bin/day4.rs
+++ aoc2024/src/bin/day4.rs
@@ -1,7 +1,36 @@
+use std::cmp::max_by_key;
 use std::fs::File;
 use std::io;
 use std::io::BufRead;
 use std::path::Path;
+use std::str::Chars;
+
+fn check_char(x: usize, y: usize, table: &Vec<Vec<char>>, mut iterator: Chars) -> bool {
+    let char = table[y][x];
+    match iterator.next() {
+        Some(next_char) => char == next_char,
+        None => true,
+    }
+}
+
+fn move_x(direction: i32, x: usize, y: usize, table: &Vec<Vec<char>>, mut iterator: Chars) -> i32 {
+    match check_char(x, y, table, iterator) {
+        true => move_x(direction + x, y)
+        false => 0
+    }
+}
+
+fn move_y(direction: i32, x: i32, y: i32) -> (i32, i32) {
+    (x, y + direction)
+}
+
+fn move_top_left_bot_right(direction: i32, x: i32, y: i32) -> (i32, i32) {
+    (x - direction, y + direction)
+}
+
+fn move_bot_left_top_right(direction: i32, x: i32, y: i32) -> (i32, i32) {
+    (x + direction, y + direction)
+}
 
 fn main() -> io::Result<()> {
     let path = Path::new("src/bin/day4.txt");
@@ -20,7 +49,9 @@ fn main() -> io::Result<()> {
 
     for (y, line) in table.iter().enumerate() {
         for (x, letter) in line.iter().enumerate() {
-            println!("{}", letter)
+            println!("{}", letter);
+            let xmas_iter = xmas.chars().into_iter();
+            let test = check_char(x, y, &table, xmas_iter);
         }
     }
 
